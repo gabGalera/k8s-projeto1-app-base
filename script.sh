b663f106b5a6d67207e885c57dfdbc612b593aad
Gabriel Galera <gabgalera@hotmail.com>
2023-06-09 12:22:49 -0300

    fix: bugs

--- script.sh
+++ script.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+
 echo "Criando as imagens..."
 
-docker build -t gabriel/k8s-project-backend:1.0 backend/.
-docker build -t gabriel/k8s-project-database:1.0 database/.
+docker build -t gabgalera/k8s-project-backend:1.0 backend/.
+docker build -t gabgalera/k8s-project-database:1.0 database/.
 
 echo "Realizando o push das imagens..."
 
-docker push gabriel/k8s-project-backend:1.0
-docker push gabriel/k8s-project-database:1.0
+docker push gabgalera/k8s-project-backend:1.0
+docker push gabgalera/k8s-project-database:1.0
+
+echo "Criando serviços no cluster kubernetes..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments..."
+
+kubectl apply -f ./deployment.yml
+
